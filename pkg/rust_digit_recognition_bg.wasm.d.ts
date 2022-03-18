2508221ef2d6cc86081261fa8d86f9b791cd1a98
Jgansaown <jgansaown0729@gmail.com>
2022-03-18 07:45:46 +0000

    Deploying to gh-pages from @ Jgansaown/rust-digit-recognition@661f63abeedda015d697e1fbd99cea3e632ecb67 🚀

--- pkg/rust_digit_recognition_bg.wasm.d.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function load_mnist_data(a: number, b: number, c: number, d: number, e: number): void;
-export function get_nth_image(a: number, b: number, c: number, d: number, e: number, f: number): void;
-export function as_png_base64_string(a: number, b: number, c: number): void;
-export function __wbindgen_add_to_stack_pointer(a: number): number;
-export function __wbindgen_malloc(a: number): number;
-export function __wbindgen_free(a: number, b: number): void;
