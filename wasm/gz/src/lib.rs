64f40a0b5d9f9faa5ef0a7d532b2d8c97dcfca40
Jason Wang <jason.chengyao.wang@gmail.com>
2022-03-21 14:01:24 +0800

    updated wasm tar name to gz

---
+++ wasm/gz/src/lib.rs
@@ -0,0 +1,11 @@
+use std::io::Read;
+use wasm_bindgen::prelude::*;
+use flate2::read::GzDecoder;
+
+#[wasm_bindgen]
+pub fn decode_gz(gzfile: &[u8]) -> Vec<u8> {
+    let mut ret = Vec::new();
+    let mut file = GzDecoder::new(gzfile);
+    file.read_to_end(&mut ret).unwrap();
+    ret
+}
