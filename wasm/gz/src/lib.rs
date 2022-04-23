9d8e170437bd6f948471d646d15d2756c6d01573
Jason Wang <jason.chengyao.wang@gmail.com>
2022-04-23 22:47:03 +0800

    added helper function for gz

--- wasm/gz/src/lib.rs
+++ wasm/gz/src/lib.rs
@@ -1,5 +1,7 @@
 use flate2::read::GzDecoder;
+use std::fs;
 use std::io::Read;
+use std::path::Path;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -8,4 +10,9 @@ pub fn decode_gz(gzfile: &[u8]) -> Vec<u8> {
     let mut file = GzDecoder::new(gzfile);
     file.read_to_end(&mut ret).unwrap();
     ret
+}
+
+pub fn decode_from_path<P: AsRef<Path>>(path: P) -> Vec<u8> {
+    let data = fs::read(path).expect("Something went wrong reading file from path");
+    decode_gz(&data)
 }
