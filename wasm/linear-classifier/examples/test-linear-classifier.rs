f81cc4e08b85d1d491ac89d41ba66e4c3378e6f1
Jason Wang <jason.chengyao.wang@gmail.com>
2022-04-26 01:34:50 +0800

    initial implementation of linear classifier

--- wasm/linear-classifier/examples/test-linear-classifier.rs
+++ wasm/linear-classifier/examples/test-linear-classifier.rs
@@ -1,7 +1,41 @@
+use std::error;
 
-use linear_classifier::test;
+use linear_classifier;
 use mnist::Dataset;
 
 fn main() {
-    test();
+    let training_dataset = Dataset::load_from_path(
+        "../files/decoded/mnist-training-data",
+        "../files/decoded/mnist-training-label",
+    );
+    let test_dataset = Dataset::load_from_path(
+        "../files/decoded/mnist-test-data",
+        "../files/decoded/mnist-test-label",
+    );
+
+    let mut lc = linear_classifier::LinearClassifier::<784>::new();
+
+    for i in 0..100 {
+        lc.train(&training_dataset);
+
+        let mut errors: usize = 0;
+        for data in test_dataset.iter() {
+            let predicts = lc.predict(data.value);
+            let predicts: Option<usize> = predicts
+                .iter()
+                .enumerate()
+                .filter_map(|(i, &p)| if p == 1.0 { Some(i) } else { None })
+                .next();
+            match predicts {
+                Some(p) => {
+                    if p != data.label as usize {
+                        errors += 1;
+                    }
+                }
+                None => errors += 1,
+            }
+            // println!("{}: {:?}", data.label, predicts);
+        }
+        println!("Iterations={}: Error Rate={}/{}={}", i, errors, test_dataset.num, (errors as f32)/(test_dataset.num as f32));
+    }
 }
--- wasm/linear-classifier/src/lib.rs
+++ wasm/linear-classifier/src/lib.rs
@@ -1,4 +1,82 @@
+use mnist::Dataset;
 
-pub fn test() {
-    println!("Hello world");
+#[derive(Debug, Clone)]
+pub struct Node {
+    weights: Vec<f32>,
+    bias: f32,
+}
+impl Node {
+    fn new(num_of_weights: usize) -> Self {
+        Self {
+            weights: vec![0.0; num_of_weights],
+            bias: 0.0,
+        }
+    }
+
+    /// Calculate the output of this node
+    ///
+    /// data must have same length as weights
+    fn output(&self, data: &[u8]) -> f32 {
+        assert_eq!(self.weights.len(), data.len());
+        let dot_product: f32 = self
+            .weights
+            .iter()
+            .zip(data)
+            .map(|(w, x)| w * (*x as f32))
+            .sum();
+        dot_product + self.bias
+    }
+
+    fn update(&mut self, change: f32, data: &[u8]) {
+        self.weights.iter_mut().zip(data).for_each(|(w, &d)| {
+            *w += change * (d as f32);
+        });
+        self.bias += change;
+    }
+}
+
+pub struct LinearClassifier<const N: usize> {
+    weights: Vec<Node>,
+    learning_rate: f32,
+}
+impl<const N: usize> LinearClassifier<N> {
+    pub fn new() -> Self {
+        Self {
+            weights: vec![Node::new(N); 10],
+            learning_rate: 0.001,
+        }
+    }
+
+    pub fn train(&mut self, dataset: &Dataset) {
+        for data in dataset.iter() {
+            let mut target: [f32; 10] = [0.0; 10];
+            target[data.label as usize] = 1.0;
+
+            let predicts: Vec<f32> = self.predict(&data.value);
+            let deltas: Vec<f32> = predicts
+                .iter()
+                .zip(&target)
+                .map(|(p, t)| self.learning_rate * (t - p))
+                .collect();
+
+            self.weights
+                .iter_mut()
+                .zip(deltas)
+                .for_each(|(node, change)| node.update(change, data.value));
+        }
+    }
+
+    fn net_input(&self, data: &[u8]) -> Vec<f32> {
+        self.weights
+            .iter()
+            .map(|weights| weights.output(data))
+            .collect()
+    }
+
+    pub fn predict(&self, data: &[u8]) -> Vec<f32> {
+        self.net_input(data)
+            .iter()
+            .map(|&output| if output >= 0.0 { 1.0 } else { 0.0 })
+            .collect()
+    }
 }
