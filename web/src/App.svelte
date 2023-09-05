0c4758ba35c96afa899bd88398ce73dd3523e970
Jason Wang <jason.chengyao.wang@gmail.com>
2023-09-05 14:16:49 -0700

    [web] Add nav bar

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-
     import ModelTrainer from "./lib/ModelTrainer.svelte";
-
-    onMount(() => {
-        // document.documentElement.setAttribute("data-theme", "light");
-        // document.documentElement.setAttribute("data-theme", "dark");
-    });
+    import NavBar from "./lib/NavBar.svelte";
 </script>
 
-<main class="container">
-    <h1>Digit Recognizer</h1>
+<NavBar />
 
+<main class="container">
     <article>
         <h2>Train your own model!</h2>
         <ModelTrainer />
---
+++ web/src/lib/NavBar.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    import GithubLogo from "../assets/github-logo.svelte";
+    import ThemeIcon from "../assets/theme-icon.svelte";
+
+    let current_theme = "dark";
+
+    onMount(() => {
+        current_theme = window.matchMedia("(prefers-color-scheme: dark)")
+            .matches
+            ? "dark"
+            : "light";
+
+        document.documentElement.setAttribute("data-theme", current_theme);
+    });
+
+    function switch_theme() {
+        current_theme = current_theme == "dark" ? "light" : "dark";
+
+        document.documentElement.setAttribute("data-theme", current_theme);
+    }
+</script>
+
+<nav style="margin: 0 1em">
+    <ul>
+        <li><strong>Digit Recognizer</strong></li>
+    </ul>
+    <ul>
+        <li>
+            <a
+                class="contrast"
+                href="https://github.com/Jgansaown/digit-recognizer"
+                ><GithubLogo /></a
+            >
+        </li>
+        <li>
+            <button
+                on:click={switch_theme}
+                style="padding: 0; background: none; border: none; box-shadow: none; color: inherit"
+                ><ThemeIcon /></button
+            >
+        </li>
+    </ul>
+</nav>
